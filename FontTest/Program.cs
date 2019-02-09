b240cfdfc0d192b308ede89ef67642992e7bdf85
Kevin Navero <knavero@gmail.com>
2019-02-08 17:13:22 -0800

    init

---
+++ FontTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+//using Typography.OpenFont;
+
+namespace FontTest {
+    class MainClass {
+        public static void Main(string[] args) {
+            var ttfPath = Path.GetFullPath(
+               Path.Combine(Directory.GetCurrentDirectory(),
+                  "..", "..", "Resources", "Typeface.ttf"));
+
+            //using (var ttf = File.OpenRead(ttfPath)) {
+            //    var fr = new OpenFontReader();
+            //    Typeface tf = fr.Read(ttf);
+            //}
+        }
+    }
+}
