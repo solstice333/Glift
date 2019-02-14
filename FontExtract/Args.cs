9895968c9ea21ddd1a511fdc97dfc0d713c51d29
Kevin Navero <knavero@gmail.com>
2019-02-14 04:07:37 -0800

    FontExtract: separate out argparsing logic

---
+++ FontExtract/Args.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using NDesk.Options;
+
+namespace ArgParse {
+    static class Args {
+        public static bool help = false;
+        public static List<char> chars = new List<char>();
+        public static int xoffset = 0;
+        public static int yoffset = 0;
+        public static string ttfPath = "";
+        public static int exst = 0;
+
+        private static OptionSet _parser = new OptionSet {
+            {
+                "c|char=", "specify a glyph to convert to .obj. " +
+                "Exit 1 if VALUE is not a single character. " +
+                "This can stack",
+                v => {
+                    if (v != null) {
+                        if (v.Length > 1) {
+                            help = true;
+                            exst = 1;
+                        }
+                        chars.Add(v[0]);
+                    }
+                }
+            },
+            {
+                "x|xoffset=", "translate the model VALUE units across " +
+                "the x axis. Exit 1 if VALUE is a non-integer",
+                v => {
+                    help = !Int32.TryParse(v, out xoffset);
+                    exst = help ? 1 : 0;
+                }
+            },
+            {
+                "y|yoffset=", "translate the model VALUE units across " +
+                "the y axis. Exit 1 if VALUE is a non-integer",
+                v => {
+                    help = !Int32.TryParse(v, out yoffset);
+                    exst = help ? 1 : 0;
+                }
+            },
+            {
+                "h|help", "show this message and exit",
+                v => {
+                    help = v != null;
+                    exst = 0;
+                }
+            }
+        };
+
+        private static void _ShowHelp(OptionSet parser) {
+            Console.WriteLine("usage: FontExtract [OPTIONS]+ TTF");
+            Console.WriteLine();
+            Console.WriteLine("convert .ttf glyphs to .obj");
+            Console.WriteLine();
+            Console.WriteLine("positional arguments:");
+            Console.WriteLine(
+                "  TTF                        path to .ttf file");
+            Console.WriteLine();
+            Console.WriteLine("optional arguments:");
+            parser.WriteOptionDescriptions(Console.Out);
+        }
+
+        private static void _ConsumePositionalArgs(List<string> args) {
+            if (args.Count != 1) {
+                help = true;
+                exst = 1;
+            }
+            else
+                ttfPath = args[0];
+        }
+
+        public static void Parse(string[] args) {
+            List<string> pos = _parser.Parse(args);
+            _ConsumePositionalArgs(pos);
+
+            if (help) {
+                _ShowHelp(_parser);
+                Environment.Exit(exst);
+            }
+        }
+    }
+}
--- FontExtract/Program.cs
+++ FontExtract/Program.cs
@@ -8,101 +8,14 @@ using Typography.Contours;
 using DrawingGL;
 using DrawingGL.Text;
 
-using NDesk.Options;
-
+using ArgParse;
 using Util;
 
-namespace FontTest {
-    static class Args {
-        public static bool help = false;
-        public static List<char> chars = new List<char>();
-        public static int xoffset = 0;
-        public static int yoffset = 0;
-        public static string ttfPath = "";
-        public static int exst = 0;
-
-        private static OptionSet _parser = new OptionSet {
-            { 
-                "c|char=", "specify a glyph to convert to .obj. " +
-                "Exit 1 if VALUE is not a single character. " +
-                "This can stack",
-                v => {
-                    if (v != null) {
-                        if (v.Length > 1) {
-                            help = true;
-                            exst = 1;
-                        }
-                        chars.Add(v[0]);
-                    }
-                }
-            },
-            {
-                "x|xoffset=", "translate the model VALUE units across " +
-                "the x axis. Exit 1 if VALUE is a non-integer",
-                v => {
-                    help = !Int32.TryParse(v, out xoffset);
-                    exst = help ? 1 : 0;
-                }
-            },
-            {
-                "y|yoffset=", "translate the model VALUE units across " +
-                "the y axis. Exit 1 if VALUE is a non-integer",
-                v => {
-                    help = !Int32.TryParse(v, out yoffset);
-                    exst = help ? 1 : 0;
-                }
-            },
-            {
-                "h|help", "show this message and exit",
-                v => {
-                    help = v != null;
-                    exst = 0;
-                }
-            }
-        };
-
-        private static void _ShowHelp(OptionSet parser) {
-            Console.WriteLine("usage: FontExtract [OPTIONS]+ TTF");
-            Console.WriteLine();
-            Console.WriteLine("convert .ttf glyphs to .obj");
-            Console.WriteLine();
-            Console.WriteLine("positional arguments:");
-            Console.WriteLine(
-                "  TTF                        path to .ttf file");
-            Console.WriteLine();
-            Console.WriteLine("optional arguments:");
-            parser.WriteOptionDescriptions(Console.Out);
-        }
-
-        private static void _ConsumePositionalArgs(List<string> args) {
-            if (args.Count != 1) {
-                help = true;
-                exst = 1;
-            }
-            else
-                ttfPath = args[0];
-        }
-
-        public static void Parse(string[] args) {
-            List<string> pos = _parser.Parse(args);
-            _ConsumePositionalArgs(pos);
-
-            if (help) {
-                _ShowHelp(_parser);
-                Environment.Exit(exst);
-            }
-        }
-    }
-
+namespace FontExtract {
     class MainClass {
         public static void Main(string[] args) {
             Args.Parse(args);
             bool allGlyphs = Args.chars.Count == 0;
-            Console.WriteLine($"want all glyphs?: {allGlyphs}");
-            Console.WriteLine($"chars: {Args.chars.ToString<char>()}");
-            Console.WriteLine($"x offset: {Args.xoffset}");
-            Console.WriteLine($"y offset: {Args.yoffset}");
-            Console.WriteLine($"path to ttf: {Args.ttfPath}");
         }
     }
 }
