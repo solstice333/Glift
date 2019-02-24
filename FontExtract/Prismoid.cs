d6d220345eb7aefcc3a7dae541a674054753f19b
Kevin Navero <knavero@gmail.com>
2019-02-24 11:38:39 -0800

    FontExtract/Prismoid.cs: Point3 props to post-edit prismoid
    when miter-ing occurs, it'll be necessary to edit the existing
    prismoid. This commit adds apis to do that

--- FontExtract/Prismoid.cs
+++ FontExtract/Prismoid.cs
@@ -18,30 +18,6 @@ namespace FontExtract {
                 throw new Non2DXYException(segment);
         }
 
-        public Point3Pair CenterLine {
-            get => _centerline;
-            set {
-                _centerline = value;
-                _vertices = null;
-            }
-        }
-
-        public int HalfThickness {
-            get => _thickness / 2;
-            set {
-                _thickness = value * 2;
-                _vertices = null;
-            }
-        }
-
-        public int Thickness {
-            get => _thickness;
-            set {
-                _thickness = value;
-                _vertices = null;
-            }
-        }
-
         private Point3[] _SquarePoints(RelativePoint point) {
             Point2 p1 = _centerline.P1.ToPoint2XY();
             Point2 p2 = _centerline.P2.ToPoint2XY();
@@ -70,21 +46,48 @@ namespace FontExtract {
             downPtAway.Z = -HalfThickness;
             downPtTowards.Z = HalfThickness;
 
-            return new Point3[] { 
-                upPtTowards, upPtAway, downPtAway, downPtTowards 
+            return new Point3[] {
+                upPtTowards, upPtAway, downPtAway, downPtTowards
             };
+        }
+
+        public Point3Pair CenterLine {
+            get => _centerline;
+            set {
+                _centerline = value;
+                _vertices = null;
+            }
+        }
+
+        public int HalfThickness {
+            get => _thickness / 2;
+            set {
+                _thickness = value * 2;
+                _vertices = null;
+            }
+        }
+
+        public int Thickness {
+            get => _thickness;
+            set {
+                _thickness = value;
+                _vertices = null;
+            }
+        }
+
+        private void _InitVertices() {
+            List<Point3> verts = new List<Point3>();
+            foreach (Point3 p in _SquarePoints(RelativePoint.P1))
+                verts.Add(p);
+            foreach (Point3 p in _SquarePoints(RelativePoint.P2))
+                verts.Add(p);
+            _vertices = verts.ToArray();
         }
 
         public IEnumerable<Point3> PointsCWStartUpperLeft {
             get {
-                if (_vertices == null) {
-                    List<Point3> verts = new List<Point3>();
-                    foreach (Point3 p in _SquarePoints(RelativePoint.P1))
-                        verts.Add(p);
-                    foreach (Point3 p in _SquarePoints(RelativePoint.P2))
-                        verts.Add(p);
-                    _vertices = verts.ToArray();
-                }
+                if (_vertices == null)
+                    _InitVertices();
                 foreach (Point3 vert in _vertices)
                     yield return vert;
             }
@@ -95,6 +98,82 @@ namespace FontExtract {
             _thickness = thickness;
             _vertices = null; 
             _centerline = segment;
+        }
+
+        public Point3 Square1UpperLeft {
+            get {
+                if (_vertices == null)
+                    _InitVertices();
+                return _vertices[0];
+            }
+            set => _vertices[0] = value;
+        }
+
+        public Point3 Square1UpperRight {
+            get {
+                if (_vertices == null)
+                    _InitVertices();
+                return _vertices[1];
+            }
+            set => _vertices[1] = value;
+        }
+
+        public Point3 Square1BottomRight {
+            get {
+                if (_vertices == null)
+                    _InitVertices();
+                return _vertices[2];
+            }
+            set => _vertices[2] = value;
+        }
+
+        public Point3 Square1BottomLeft {
+            get {
+                if (_vertices == null)
+                    _InitVertices();
+                return _vertices[3];
+            }
+            set => _vertices[3] = value;
+        }
+
+        public Point3 Square2UpperLeft {
+            get {
+                if (_vertices == null)
+                    _InitVertices();
+                return _vertices[4];
+            }
+            set => _vertices[4] = value;
+        }
+
+        public Point3 Square2UpperRight {
+            get {
+                if (_vertices == null)
+                    _InitVertices();
+                return _vertices[5];
+            }
+            set => _vertices[5] = value;
+        }
+
+        public Point3 Square2BottomRight {
+            get {
+                if (_vertices == null)
+                    _InitVertices();
+                return _vertices[6];
+            }
+            set => _vertices[6] = value;
+        }
+
+        public Point3 Square2BottomLeft {
+            get {
+                if (_vertices == null)
+                    _InitVertices();
+                return _vertices[7];
+            }
+            set => _vertices[7] = value;
+        }
+
+        public void ResetVertices() {
+            _vertices = null;
         }
     }
 }
